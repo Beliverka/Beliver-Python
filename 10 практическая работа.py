d8a7ba5716717b6af9c07aacba0d4b179f441e17
Beliverka <sobol.kirill34@gmail.com>
2024-12-03 19:50:55 +0300

    Add files via upload

---
+++ 10 практическая работа.py
@@ -0,0 +1,3 @@
+with open('Соболев К.М. У-244 работа №9 4 вариант 1 задание.ру', 'r') as fin:
+    with open('Соболев К.М. У-244 работа №9 4 вариант 1 задание.py', 'w') as fout:
+        fout.writelines(fin.readlines())
