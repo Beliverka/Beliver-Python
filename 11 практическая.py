974c6ac55a536663ab523dde8f1eb560418d2856
Beliverka <sobol.kirill34@gmail.com>
2024-12-03 19:49:37 +0300

    Add files via upload

---
+++ 11 практическая.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from pprint import pprint
+
+username = "NixOS"
+url = f"https://api.github.com/users/{username}"
+# делаем запрос и возвращаем json
+user_data1 = requests.get(url).json()
+
+print(user_data1)
+print('-'*25)
+s1 = json.dumps(user_data1)
+user_data = json.loads(s1)
+info = {
+        'company'   : (user_data["company"]   ),
+        'created_at': (user_data["created_at"]),
+        'email'     : (user_data["email"]     ),
+        'id'        : (user_data["id"]        ),
+        'name'      : (user_data["name"]      ),
+        'url'       : (user_data["url"]       )
+        }
+pprint(info)
+
+with open('Json.txt', 'w') as file_json:
+  json.dump(info,file_json)
+
